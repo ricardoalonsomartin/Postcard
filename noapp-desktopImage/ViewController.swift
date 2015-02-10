6fd91647daeca77846a89a2dab6cde83015671e1
Ricardo Alonso Martín <MacbookAir@MacBook-Air-de-Ricardo.local>
2015-02-10 23:36:56 +0100

    Initial Commit

---
+++ noapp-desktopImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  noapp-desktopImage
+//
+//  Created by Ricardo Alonso Martín on 10/2/15.
+//  Copyright (c) 2015 Ricardo Alonso Martín. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
