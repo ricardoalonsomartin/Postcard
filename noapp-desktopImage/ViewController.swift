3b12ed8c00590053d03154b2453155f369f58eaa
Ricardo Alonso Martín <ricardo.alonso.martin@gmail.com>
2015-02-13 01:38:09 +0100

    Changing labels after actions with buttons

--- noapp-desktopImage/ViewController.swift
+++ noapp-desktopImage/ViewController.swift
@@ -13,9 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
-    @IBOutlet weak var asdf: UIButton!
     @IBOutlet weak var mailButton: UIButton!
-        
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameAndMessageLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -31,11 +34,30 @@ class ViewController: UIViewController {
             messageLabel.text = enterMessageTextField.text;
             messageLabel.hidden = false;
             messageLabel.textColor = UIColor.redColor();
+        nameLabel.text = enterNameTextField.text;
+        nameLabel.hidden = false;
+        nameLabel.textColor = UIColor.yellowColor();
         
             enterMessageTextField.text = "";
             enterMessageTextField.resignFirstResponder();
+            enterNameTextField.text = "";
+            enterNameTextField.resignFirstResponder();
+
             mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
     
+    @IBAction func submitButtonPressed(sender: UIButton) {
+        var aux:String = "Hi \(enterNameTextField.text)!\n \(enterMessageTextField.text)";
+        
+        nameAndMessageLabel.hidden = false;
+        nameAndMessageLabel.text = aux;
+
+        enterNameTextField.text = "";
+        enterMessageTextField.text = "";
+        enterMessageTextField.resignFirstResponder();
+        enterNameTextField.resignFirstResponder();
+        
+        submitButton.setTitle("Submited", forState: UIControlState.Normal);
+    }
 }
 
